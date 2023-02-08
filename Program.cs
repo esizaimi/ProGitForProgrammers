e508a4b5d5148c335e4b3eadbb94a3d222cd9c16
ezzyezzy <ezaimi89@gmail.com>
2023-02-08 10:15:30 +0100

    Index Added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace ProGitForProgrammers
+
+{
+    class Program
+    {
+     static void Main (string[] args)   
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("I just added this in Visual Studio");
+
+    }
+    }
+    
+}
